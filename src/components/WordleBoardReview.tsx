fa4b3b21f159bf831e6242f9cee6fb5335cd282c
June Lee <june87lee@gmail.com>
2026-02-09 19:47:17 -0800

    FIX: useeffect begone!

--- src/components/WordleBoardReview.tsx
+++ src/components/WordleBoardReview.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { DEFAULT_COLOR_MAP } from "../assets/constants";
 import useWordle from "../hooks/useWordle";
 
